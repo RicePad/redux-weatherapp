138713be9c2dc29a76d18307ca4a9ae60f9d59e8
RiskyClick <jonn.thinkrtc@gmail.com>
2017-08-29 04:09:31 +0000

    first commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { BOOK_SELECTED } from '../constants'
 // export function selectBook(book){
 //     console.log('a book has been selected:', book.title);
    
@@ -14,7 +15,7 @@ export function selectBook(book){
     // console.log('a book has been selected:', book.title);
    
    return {
-       type: 'BOOK_SELECTED',
+       type: BOOK_SELECTED,
        payload: book
        
        
