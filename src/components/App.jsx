56c7ae2e06b4047203aa181b64b29ee9eef22169
RiskyClick <jonn.thinkrtc@gmail.com>
2017-08-30 18:00:10 +0000

    add App component to application

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+ 
+ render(){
+   return (
+       <div>Hello World from app component</div>
+       
+       )}   
+    
+}
+
+export default App;
